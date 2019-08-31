f4a81594c1f032d7734e28a8f547bc0c9612a4ef
mrEsmaeili <lvlr.esmaeili@gmail.com>
2019-08-31 22:02:25 +0300

    initial production ready

--- server/index.js
+++ server/index.js
@@ -27,5 +27,15 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up prod assets like main.js / main.css
+  app.use(express.static('client/build'));
+  //Express will serve index.html file if it dosen't recognize the route
+  app.get('*', (req, res) => {
+    const path = require('path');
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
