70ba1000c2b9dd90e2bb5fd170d205e2a64a8c16
mrEsmaeili <lvlr.esmaeili@gmail.com>
2019-09-02 21:04:56 +0300

    stripe setup

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 
 require('./models/Users');
+require('./models/Survey');
 require('./services/passport');
 
 mongoose.connect(keys.mongoURI);
@@ -26,6 +27,7 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
+require('./routes/surveyRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
   // Express will serve up prod assets like main.js / main.css
